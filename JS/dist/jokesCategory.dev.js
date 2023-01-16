0e871b262d89c35945d5af91d10783b5ea225c52
Rahmath007 <rahmath207@gmail.com>
2023-01-16 14:55:57 +0000

    fixed button were displayed left side of the page. now it showing in the middle.
    changed footer position from fixed
    nothing

--- JS/dist/jokesCategory.dev.js
+++ JS/dist/jokesCategory.dev.js
@@ -1,8 +1,15 @@
 "use strict";
 
+/**
+ * Fetches a random joke from the chucknorris.io API and displays it on the webpage
+ * @param {string} cat - The category of the joke
+ * @param {string} target - The ID of the element on the webpage where the joke will be displayed
+ */
 function getJokes(cat, target) {
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", "https://api.chucknorris.io/jokes/random?category=" + cat);
+  // Create a new instance of the XMLHttpRequest object
+  var xhr = new XMLHttpRequest(); // Open a GET request to the API endpoint for jokes with the specified category
+
+  xhr.open("GET", "https://api.chucknorris.io/jokes/random?category=" + cat); // Event handler for when the request finishes
 
   xhr.onload = function () {
     if (xhr.readyState === 4) {
@@ -25,16 +32,44 @@ function getJokes(cat, target) {
 
   xhr.send();
 }
+/*
 
 document.getElementById("dev").addEventListener("click", function (e) {
+    getJokes(e.target.id, e.target.dataset.target)
+})
+
+document.getElementById("animal").addEventListener("click", function (e) {
+    getJokes(e.target.id, e.target.dataset.target)
+})
+
+document.getElementById("science").addEventListener("click", function (e) {
+    getJokes(e.target.id, e.target.dataset.target)
+})
+
+document.getElementById("explicit").addEventListener("click", function (e) {
+    getJokes(e.target.id, e.target.dataset.target)
+})
+
+*/
+
+/**
+ * Adds click event listeners to the buttons that fetch jokes from the API and display them on the webpage
+ */
+
+
+document.getElementById("dev").addEventListener("click", function (e) {
+  // Pass the button's id and data-target attribute as arguments to the getJokes function
   getJokes(e.target.id, e.target.dataset.target);
 });
 document.getElementById("animal").addEventListener("click", function (e) {
+  // Pass the button's id and data-target attribute as arguments to the getJokes function
   getJokes(e.target.id, e.target.dataset.target);
 });
 document.getElementById("science").addEventListener("click", function (e) {
+  // Pass the button's id and data-target attribute as arguments to the getJokes function
   getJokes(e.target.id, e.target.dataset.target);
 });
 document.getElementById("explicit").addEventListener("click", function (e) {
+  // Pass the button's id and data-target attribute as arguments to the getJokes function
   getJokes(e.target.id, e.target.dataset.target);
 });
--- JS/jokesCategory.js
+++ JS/jokesCategory.js
@@ -1,6 +1,18 @@
+/**
+ * Fetches a random joke from the chucknorris.io API and displays it on the webpage
+ * @param {string} cat - The category of the joke
+ * @param {string} target - The ID of the element on the webpage where the joke will be displayed
+ */
+
 function getJokes(cat, target) {
+    
+    // Create a new instance of the XMLHttpRequest object
     const xhr = new XMLHttpRequest();
+
+    // Open a GET request to the API endpoint for jokes with the specified category
     xhr.open("GET", "https://api.chucknorris.io/jokes/random?category=" + cat);
+    
+    // Event handler for when the request finishes
     xhr.onload = function () {
         if (xhr.readyState === 4) {
             if (xhr.status === 200) {
@@ -20,7 +32,7 @@ function getJokes(cat, target) {
     };
     xhr.send();
 }
-
+/*
 
 document.getElementById("dev").addEventListener("click", function (e) {
     getJokes(e.target.id, e.target.dataset.target)
@@ -37,3 +49,28 @@ document.getElementById("science").addEventListener("click", function (e) {
 document.getElementById("explicit").addEventListener("click", function (e) {
     getJokes(e.target.id, e.target.dataset.target)
 })
+
+*/
+
+/**
+ * Adds click event listeners to the buttons that fetch jokes from the API and display them on the webpage
+ */
+document.getElementById("dev").addEventListener("click", function (e) {
+    // Pass the button's id and data-target attribute as arguments to the getJokes function
+    getJokes(e.target.id, e.target.dataset.target)
+});
+
+document.getElementById("animal").addEventListener("click", function (e) {
+    // Pass the button's id and data-target attribute as arguments to the getJokes function
+    getJokes(e.target.id, e.target.dataset.target)
+});
+
+document.getElementById("science").addEventListener("click", function (e) {
+    // Pass the button's id and data-target attribute as arguments to the getJokes function
+    getJokes(e.target.id, e.target.dataset.target)
+});
+
+document.getElementById("explicit").addEventListener("click", function (e) {
+    // Pass the button's id and data-target attribute as arguments to the getJokes function
+    getJokes(e.target.id, e.target.dataset.target)
+});
