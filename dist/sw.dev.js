59f411fb1f07988a21fbe9940655731521a69ec4
Rahmath007 <rahmath207@gmail.com>
2023-01-18 10:41:19 +0000

    fixed caching error. error was CSS files fore some reason are not caching.

--- dist/sw.dev.js
+++ dist/sw.dev.js
@@ -1,14 +1,21 @@
 "use strict";
 
+// CSS files are not caching!!
 //install service worker
+var CacheName = 'siteStatic';
+var files = ['/', '/HTML/index.html', '/HTML/JokesCategory.html', '/HTML/search.html', '/HTML/about.html', '/HTML/contactUs.html', '/js/app.js', '/JS/jokesCategory.js', '/JS/script.js', '/JS/search.js', '/img/chucknorris.png', '/img/images.jpeg', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css'];
 self.addEventListener('install', function (evt) {
-  console.log('service worker has been installed');
+  //console.log('service worker has been installed');
+  evt.waitUntil(caches.open(CacheName).then(function (cache) {
+    console.log('caching shell assets');
+    cache.addAll(files)["catch"](function (error) {
+      console.log("Error adding assets to cache", error);
+    });
+  }));
 }); // activate event
 
-self.addEventListener('activate', function (evt) {
-  console.log('service worker has been activated');
+self.addEventListener('activate', function (evt) {//console.log('service worker has been activated');
 }); //fetch event
 
-self.addEventListener('fetch', function (evt) {
-  console.log('fetch event', evt);
+self.addEventListener('fetch', function (evt) {// console.log('fetch event', evt);
 });
--- sw.js
+++ sw.js
@@ -1,15 +1,44 @@
+// CSS files are not caching!!
+
 //install service worker
+
+const CacheName = 'siteStatic';
+const files = [
+    '/',
+    '/HTML/index.html',
+    '/HTML/JokesCategory.html',
+    '/HTML/search.html',
+    '/HTML/about.html',
+    '/HTML/contactUs.html',
+    '/js/app.js',
+    '/JS/jokesCategory.js',
+    '/JS/script.js',
+    '/JS/search.js',
+    '/img/chucknorris.png',
+    '/img/images.jpeg',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css'
+
+];
+
 self.addEventListener('install', evt => {
-    console.log('service worker has been installed');
-});
+    //console.log('service worker has been installed');
+    evt.waitUntil(
+        caches.open(CacheName).then((cache) => {
+            console.log('caching shell assets');
+            cache.addAll(files).catch((error) => {
+                console.log("Error adding assets to cache", error);
+            });
+        })
+    )});
+    
 
 // activate event
 
 self.addEventListener('activate', evt => {
-    console.log('service worker has been activated');
+    //console.log('service worker has been activated');
 });
 
 //fetch event
 self.addEventListener('fetch', evt => {
-    console.log('fetch event', evt);
+   // console.log('fetch event', evt);
 });
